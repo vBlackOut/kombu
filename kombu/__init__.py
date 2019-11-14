63288ba4e34be908a9259ac5220886f4b5600228
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2019-11-14 01:28:43 +0100

    Update __init__.py

--- kombu/__init__.py
+++ kombu/__init__.py
@@ -12,7 +12,7 @@ version_info_t = namedtuple(
 )
 
 VERSION = version_info_t(3, 0, 37, '', '')
-__version__ = '{0.major}.{0.minor}.{0.micro}{0.releaselevel}'.format(VERSION)
+__version__ = '{major}.{minor}.{micro}{releaselevel}'.format(VERSION)
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
 __homepage__ = 'https://kombu.readthedocs.io'
